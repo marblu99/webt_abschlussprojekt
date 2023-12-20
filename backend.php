293c4014808fb3a27be9b17c3107d52ee011cc54
marblu99 <mablumrc@gmail.com>
2023-12-20 14:35:42 +0100

    JS an neues HTML angepasst

--- backend.php
+++ backend.php
@@ -1,8 +1,4 @@
 <?php
-// Empfange die JSON-Daten von der POST-Anfrage
-$jsonData = file_get_contents('php://input');
-$data = json_decode($jsonData);
-
 // Verbindungsparameter zur MySQL-Datenbank
 $servername = "localhost";
 $username = "root";
@@ -19,90 +15,10 @@ if ($conn->connect_error) {
 
 $response = array(); // Array für die Serverantwort
 
-// Funktion zum Einfügen von Daten in die Datenbank
-function insertData($conn, $benutzer, $fach, $note)
-{
-    // SQL-Abfrage zum Einfügen der Daten in die Tabelle notenDatenbank mit Prepared Statement
-    $sql = "INSERT INTO notenDatenbank (Benutzer, Fach, Note) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    if ($stmt === false) {
-        return array('error' => 'Fehler beim Vorbereiten der Eintragsabfrage: ' . $conn->error);
-    }
-    $stmt->bind_param("sss", $benutzer, $fach, $note);
-
-    // Die SQL-Abfrage ausführen
-    if ($stmt->execute() !== TRUE) {
-        return array('error' => 'Fehler beim Eintragen der Daten: ' . $conn->error);
-    }
-
-    // Prepared Statement schließen
-    $stmt->close();
-
-    return null; // Erfolg
-}
-
-// Funktion zum Berechnen des Notendurchschnitts pro Fach
-function calculateAverage($conn, $benutzer)
-{
-    $sqlDurchschnitt = "SELECT Benutzer, Fach, AVG(Note) AS Durchschnitt, GROUP_CONCAT(Note) AS Noten FROM notenDatenbank WHERE Benutzer = ? GROUP BY Fach";
-    $stmtDurchschnitt = $conn->prepare($sqlDurchschnitt);
-    if ($stmtDurchschnitt === false) {
-        return array('error' => 'Fehler beim Vorbereiten der Durchschnittsabfrage: ' . $conn->error);
-    }
-    $stmtDurchschnitt->bind_param("s", $benutzer);
-    $stmtDurchschnitt->execute();
-    $resultDurchschnitt = $stmtDurchschnitt->get_result();
-
-    $averages = array();
-
-    if ($resultDurchschnitt !== FALSE) {
-        if ($resultDurchschnitt->num_rows > 0) {
-            while ($rowDurchschnitt = $resultDurchschnitt->fetch_assoc()) {
-                $fachDurchschnitt = array(
-                    'Benutzer' => $rowDurchschnitt['Benutzer'],
-                    'Fach' => $rowDurchschnitt['Fach'],
-                    'Durchschnitt' => $rowDurchschnitt['Durchschnitt'],
-                    'Noten' => explode(',', $rowDurchschnitt['Noten'])
-                );
-
-                // Das Fachdurchschnittsobjekt zum Serverantwort-Array hinzufügen
-                $averages[] = $fachDurchschnitt;
-            }
-        }
-    }
-
-    // Prepared Statement schließen
-    $stmtDurchschnitt->close();
-
-    return $averages;
-}
-
-// Falls Daten vorhanden sind, diese in die Datenbank eintragen
-if (!empty($data->Faecher)) {
-    $benutzer = $data->Benutzer;
-    $faecher = $data->Faecher;
-
-    foreach ($faecher as $fach) {
-        $fachName = $fach->Fach;
-        $noten = $fach->Noten;
-
-        foreach ($noten as $note) {
-            $error = insertData($conn, $benutzer, $fachName, $note);
-            if ($error !== null) {
-                $response[] = $error;
-            }
-        }
-    }
-}
-
-// Notendurchschnitte pro Fach berechnen
-if (!empty($benutzer)) {
-    $response = array_merge($response, calculateAverage($conn, $benutzer));
-}
-
-// Abrufen aller Benutzernamen und zum Response hinzufügen mit Prepared Statement
+// Abrufen aller Benutzernamen mit Prepared Statement
 $sqlBenutzer = "SELECT DISTINCT Benutzer FROM notenDatenbank";
 $stmtBenutzer = $conn->prepare($sqlBenutzer);
+
 if ($stmtBenutzer === false) {
     $response[] = array('error' => 'Fehler beim Vorbereiten der Benutzerabfrage: ' . $conn->error);
 } else {
--- js/script.js
+++ js/script.js
@@ -1,3 +1,146 @@
+// Eventlistener für Funktionen die beim Laden der Seite ausgeführt werden sollen
+document.addEventListener("DOMContentLoaded", function () {
+    benutzerAuswahl(false);
+    bereitsErstellteBenutzerAbfragen();
+});
+
+// Benutzerfelder gemäss Auswahl anzeigen
+function benutzerAuswahl(neuerBenutzer) {
+    let usernameInput = document.getElementById('username');
+    let benutzerDropdown = document.getElementById('benutzerDropdown');
+    let benutzerDropdownLabel = document.querySelector('label[for="benutzerDropdown"]');
+
+    if (neuerBenutzer) {
+        benutzerDropdownLabel.style.display = 'none';
+        benutzerDropdown.style.display = 'none';
+        usernameInput.style.display = 'block';
+    } else {
+        benutzerDropdownLabel.style.display = 'block';
+        benutzerDropdown.style.display = 'block';
+        usernameInput.style.display = 'none';
+    }
+}
+
+// AJAX-Anfrage, um Benutzernamen abzurufen
+function bereitsErstellteBenutzerAbfragen() {
+
+    let xhr = new XMLHttpRequest();
+    let url = 'backend.php';
+    xhr.open('GET', url, true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let responseData = JSON.parse(xhr.responseText);
+
+            if ('benutzerListe' in responseData) {
+                // Dropdown-Element aus dem HTML-Dokument auswählen
+                let dropdown = document.getElementById('benutzerDropdown');
+
+                // Schleife durch die Benutzerliste und fügt Optionen zum Dropdown hinzu
+                responseData.benutzerListe.forEach(function (benutzer) {
+                    var option = document.createElement('option');
+                    option.value = benutzer;
+                    option.text = benutzer;
+                    dropdown.add(option);
+                });
+            } else if ('error' in responseData) {
+                console.error('Fehler beim Abrufen der Benutzerliste: ' + responseData.error);
+            } else {
+                console.error('Unerwartete Antwortformat.');
+            }
+        } else {
+            console.error('Fehler beim Abrufen der Benutzerliste.');
+        }
+    };
+
+    xhr.send();
+
+};
+
+function anzeigenEingabeNotentabelle() {
+    let tabelle = document.getElementById('notenTabelleEinträge');
+    tabelle.style.display = 'block';
+
+    console.log(letzteZeile)
+}
+
+function zeileTabelleEinfügen() {
+
+    let tabelle = document.getElementById('notenTabelleEinträge');
+
+    let neuesFach = document.createElement('input');
+    neuesFach.type = 'text';
+    let neueNote = document.createElement('input');
+    neueNote.type = 'number';
+    let neuerButton = document.createElement('button');
+    neuerButton.innerText = 'Note hinzufügen';
+
+    if (tabelle.style.display === 'none') {
+
+        tabelle.style.display = 'block';
+
+        // Erstelle die Titelzeile
+        let titelZeile = tabelle.createTHead().insertRow();
+        let fachHeader = titelZeile.insertCell(0);
+        fachHeader.innerText = 'Fach';
+
+        let noteHeader = titelZeile.insertCell(1);
+        noteHeader.innerText = 'Note';
+
+        // Füge eine normale Zeile zum tbody hinzu
+        let normaleZeile = tabelle.insertRow();
+
+        // Füge die Zellen für Fach, Note und Button hinzu
+        let fachZelle = normaleZeile.insertCell(0);
+        fachZelle.appendChild(neuesFach);
+
+        let noteZelle = normaleZeile.insertCell(1);
+        noteZelle.appendChild(neueNote);
+
+        let buttonZelle = normaleZeile.insertCell(2);
+        buttonZelle.appendChild(neuerButton);
+
+
+    } else {
+
+        let normaleZeile = tabelle.insertRow();
+
+        // Füge die Zellen für Fach, Note und Button hinzu
+        let fachZelle = normaleZeile.insertCell(0);
+        fachZelle.appendChild(neuesFach);
+
+        let noteZelle = normaleZeile.insertCell(1);
+        noteZelle.appendChild(neueNote);
+
+        let buttonZelle = normaleZeile.insertCell(2);
+        buttonZelle.appendChild(neuerButton);
+
+    }
+}
+
+
+
+/*
+
+
+
+
+// Titel der Fächereingabe, Noten anzeigen und Inputfelder erstellen
+function fächerAnzeigen() {
+    document.getElementById('Fächer').style.display = 'block';
+    document.getElementById('Noten').style.display = 'block';
+    ändernNotendurchschnittButton();
+    neuesFachErstellen();
+}
+
+// Name des Buttons ändern
+function ändernNotendurchschnittButton() {
+    document.getElementById('notenDurchschnittButton').innerText = 'Neuer Notendurchschnitt berechnen';
+}
+
+
+
+
 // Globale Variablen
 let fachZähler = 0;
 let wurdeBereitsAufgerufen = false;
@@ -172,3 +315,5 @@ function userFachNotenObjekt() {
     // Anfrage senden
     xhr.send(jsonData);
 }
+
+*/
